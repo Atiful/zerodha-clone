8c372bc7273d5d6a780cb655d85b58479d74ba8b
Atiful haque <mdatifulhaque26@gmail.com>
2025-02-14 19:04:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,7 @@ app.use(
       'http://localhost:3000',
       'http://localhost:5174/',
       // 'https://investwise-backend.onrender.com',
-      'https://inverstwise-backend.onrender.com
+      'https://inverstwise-backend.onrender.com',
       // 'https://your-domain.com', // Add production URL here
     ],
   }),
